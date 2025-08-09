6bcc68aa8cd0ef6c2e652e285323c009875a5e02
Gabriel Rodrigues <118090628+GabrielR255@users.noreply.github.com>
2025-08-09 15:56:05 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,8 +144,8 @@ function App() {
           </div>
         </div>
 
-        <div className="flex justify-center items-center bg-gray-800 p-2 sm:p-5 w-full sm:w-[860px] mt-7 rounded-2xl">
-          <h1 className="text-lg sm:text-xl text-white text-center">DESAFIO - SPEED CLICK DINÂMICO</h1>
+        <div className="flex justify-center items-center bg-gray-800 p-2 sm:p-5 w-full sm:w-[860px] mt-5 rounded-2xl">
+          <h1 className="text-sm sm:text-xl text-white text-center">DESAFIO - SPEED CLICK DINÂMICO</h1>
         </div>
       </div>
     </div>
